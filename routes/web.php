d94b0dabb5364cad46e0989ef24d298e0e351f98
seyou-U <90487620+seyou-U@users.noreply.github.com>
2024-10-13 13:39:52 +0900

    Laravel Breeze / パッケージインストール

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\HelloController;
-use App\Http\Controllers\PeopleController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// 特定のルートに対してミドルウェアを割り当てたい時は、middlewareメソッドを呼び出す
-Route::get('hello', [HelloController::class, 'index'])
-    ->middleware('group-name');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('people', [PeopleController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
